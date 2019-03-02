2600d20714ddff97253f9537be4407701b522001
vijayprakashreddy Billapati <prakashvijay54@gmail.com>
2019-03-02 05:41:29 +0530

    Update JavaRegex.java

--- JavaRegex.java
+++ JavaRegex.java
@@ -1,3 +1,7 @@
+/*IP address is a string in the form "A.B.C.D", where the value of A, B, C, and D may range from 0 to 255. 
+Leading zeros are allowed. 
+The length of A, B, C, or D can't be greater than 3.*/
+
 import java.util.Scanner;
 
 public class JavaRegex {
